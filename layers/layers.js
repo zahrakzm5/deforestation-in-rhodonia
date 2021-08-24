2c35ae5c69036dcbdaa79dbb68ac717d9393d22a
zahrakzm5 <88771590+zahrakzm5@users.noreply.github.com>
2021-08-24 10:49:01 +0200

    Add files via upload

---
+++ layers/layers.js
@@ -0,0 +1,133 @@
+
+ol.proj.proj4.register(proj4);
+ol.proj.get("EPSG:32620").setExtent([344475.000000, -1188885.000000, 502155.000000, -1083765.000000]);
+
+
+
+
+
+
+
+
+var wms_layers = [];
+
+var lyr_RondoniaDeforestation_19840624_redres_1200x800px_0 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_19840624_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_19840624_redres_1200x800px_0.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_19950623_redres_1200x800px_1 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_19950623_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_19950623_redres_1200x800px_1.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_19890708_redres_1200x800px_2 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_19890708_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_19890708_redres_1200x800px_2.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_19950623_redres_1200x800px_3 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_19950623_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_19950623_redres_1200x800px_3.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_20000706_redres_1200x800px_4 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_20000706_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_20000706_redres_1200x800px_4.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_20050602_redres_1200x800px_5 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_20050602_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_20050602_redres_1200x800px_5.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_20100515_redres_1200x800px_6 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_20100515_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_20100515_redres_1200x800px_6.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_20150630_redres_1200x800px_7 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_20150630_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_20150630_redres_1200x800px_7.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+var lyr_RondoniaDeforestation_20200713_redres_1200x800px_8 = new ol.layer.Image({
+                            opacity: 1,
+                            title: "Rondonia-Deforestation_20200713_redres_1200x800px",
+                            
+                            
+                            source: new ol.source.ImageStatic({
+                               url: "./layers/RondoniaDeforestation_20200713_redres_1200x800px_8.png",
+    attributions: ' ',
+                                projection: 'EPSG:32620',
+                                alwaysInRange: true,
+                                imageExtent: [344475.000000, -1188885.000000, 502155.000000, -1083765.000000]
+                            })
+                        });
+
+lyr_RondoniaDeforestation_19840624_redres_1200x800px_0.setVisible(true);lyr_RondoniaDeforestation_19950623_redres_1200x800px_1.setVisible(true);lyr_RondoniaDeforestation_19890708_redres_1200x800px_2.setVisible(true);lyr_RondoniaDeforestation_19950623_redres_1200x800px_3.setVisible(true);lyr_RondoniaDeforestation_20000706_redres_1200x800px_4.setVisible(true);lyr_RondoniaDeforestation_20050602_redres_1200x800px_5.setVisible(true);lyr_RondoniaDeforestation_20100515_redres_1200x800px_6.setVisible(true);lyr_RondoniaDeforestation_20150630_redres_1200x800px_7.setVisible(true);lyr_RondoniaDeforestation_20200713_redres_1200x800px_8.setVisible(true);
+var layersList = [lyr_RondoniaDeforestation_19840624_redres_1200x800px_0,lyr_RondoniaDeforestation_19950623_redres_1200x800px_1,lyr_RondoniaDeforestation_19890708_redres_1200x800px_2,lyr_RondoniaDeforestation_19950623_redres_1200x800px_3,lyr_RondoniaDeforestation_20000706_redres_1200x800px_4,lyr_RondoniaDeforestation_20050602_redres_1200x800px_5,lyr_RondoniaDeforestation_20100515_redres_1200x800px_6,lyr_RondoniaDeforestation_20150630_redres_1200x800px_7,lyr_RondoniaDeforestation_20200713_redres_1200x800px_8];
